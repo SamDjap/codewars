f7e0a1e34ff838737479e674fced54bb808d0b25
Sam Djap <sdjapdev@gmail.com>
2022-05-25 23:05:24 -0400

    codewars 5/24

---
+++ solution5-24.js
@@ -0,0 +1,8 @@
+//   Complete the function to return true if the two arguments given are anagrams of each other; return false otherwise.
+
+var isAnagram = function(test, original) {
+    let t = test.toLowerCase().split('').sort().join('');
+    let o = original.toLowerCase().split('').sort().join('');
+    return (t==o) ? true:false ;
+  };
+  
