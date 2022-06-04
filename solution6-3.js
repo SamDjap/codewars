85761d305b9a9ccd04949b08baa39ac813167ba6
Sam Djap <sdjapdev@gmail.com>
2022-06-03 22:41:38 -0400

    7Kyu // determine sum of array as odd or even. Reduce method used

--- solution6-3.js
+++ solution6-3.js
@@ -3,4 +3,16 @@
 function combat(health, damage) {
     return health > damage ? health - damage : 0
   }
-  
+
+
+
+//   Given a list of integers, determine whether the sum of its elements is odd or even.
+
+// Give your answer as a string matching "odd" or "even".
+
+// If the input array is empty consider it as: [0] (array with a zero).
+function oddOrEven(array) {
+    let result = array.reduce((a,c) => a+c,0)
+    return result%2 === 0 || result===0 ? 'even' : 'odd'
+ 
+ }
